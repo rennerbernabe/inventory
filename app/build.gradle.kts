dccc5f9b2fddf853e3f07335d294b2c0bae7b4b0
rennerbernabe_ <renner.bernabe@cartrack.com>
2024-01-31 15:38:53 +0800

    Add db test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,8 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
+    // Testing
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
